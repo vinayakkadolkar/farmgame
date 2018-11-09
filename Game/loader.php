7206e2634069052e3bd046db59ebc7aaa358dbd8
Vinayak <vinayak.kadolkar89@gmail.com>
2018-11-09 20:43:35 +0530

    Loader and start creating game

---
+++ Game/loader.php
@@ -0,0 +1,77 @@
+<?php
+namespace Game;
+
+class Loader    
+{
+    private $default_configs_file="../settings/fgconfigs.json";
+    public $save_file="../settings/fgsave.json";
+    public $error_message = array();
+    /**
+	 * Function __Construct
+	 *
+	*/
+    public function __construct()
+    {
+
+    }
+
+    public function loadDefaults()
+    {
+        # code...
+    }
+
+    public function create_new_game()
+    {
+        $defaults=array();
+        try{
+            $defaults=json_decode(file_get_contents($this->default_configs_file));
+        }catch(exception $e){
+            $this->error_message[]="failed to Load default Game Configurations";
+            return false;
+        }
+        $gameSettings=array();
+        $gameSettings['turn']=0;
+        $gameSettings['turn_cost']=$defaults['turn_cost'];
+        $gameSettings['max_turns']=$defaults['maz_turns'];
+        $charactersList=$this->create_characters($defaults["characters_list"]);
+        if(!$charactersList){
+            $this->error_message[]="Failed to Load Characters in Game";
+            return false;
+        }
+        $gameSettings['characters']=$charactersList;
+        try{
+            file_put_contents($this->save_file,json_encode($gameSettings));
+        }catch(exception $e){
+            $this->error_message[]="Failed to save the Game";
+            return false;
+        }
+
+    }
+
+    private function create_characters($character_raw_list)
+    {   
+        if(empty($character_raw_list)){
+            return false;
+        }
+        $characters=array();
+        foreach ($character_raw_list as $char_code  => $character) 
+        {
+            for ($numbers=1; $numbers <= $character['numbers']; $numbers++) 
+            { 
+                $characters[]=array(
+                    'code'=>$char_code."_".$numbers,
+                    'type'=>$char_code,
+                    'name'=>$character['name']."_".$numbers,
+                    'tolarance'=>$character['tolarance'],
+                    'tolarated'=>0,
+                    'game_end_on_death'=>$character['game_end_on_death'],
+                    'dead'=>false,
+                    'death_turn'=>0
+                );
+            }
+        }
+        return $characters;
+    }
+}
+
+
